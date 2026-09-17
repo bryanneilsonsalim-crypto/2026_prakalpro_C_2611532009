b27d296bbcd86071e63600a3e37993ab61556c43
bryanneilsonsalim-crypto <bryanneilsonsalim@gmail.com>
2026-09-17 17:26:15 +0700

    pekan2

--- pekan2/Tugas2_2611532009.py
+++ pekan2/Tugas2_2611532009.py
@@ -1,15 +1,25 @@
-print("=== SISTEM REGISTRASI PRAKTIKUM ALPRO 2026 ===")
-Nama_2009 = input("Masukkan Nama Mahasiswa: ")
-Jenis_Kelamin_2009 = input('L/P: ')
-Umur_2009 = int(input("Masukkan Umur: "))
-Skor_tes_awal_2009 = float(input("Masukkan Skor Tes Awal: "))
+from typing import Final
+
+#Menentukan Konstanta
+batas_nilai_2009 : Final = 75.0
+
+#Penggunaan Multistring 
 alamat_2009 = """
 Jl. Kampus Unand
 Kecamatan Pauh,
 Kota Padang
 """
+# Tipe data Complex
 token_sinyal_2009 = 100 + 3j
-batas_nilai_2009 = 75
+
+print("=== SISTEM REGISTRASI PRAKTIKUM ALPRO 2026 ===")
+Nama_2009 = input("Masukkan Nama Mahasiswa: ")
+Jenis_Kelamin_2009 = input('L/P: ')
+Umur_2009 = int(input("Masukkan Umur: "))
+Skor_tes_awal_2009 = float(input("Masukkan Skor Tes Awal: "))
+
+
+
 hasil_kelulusan_2009 = Skor_tes_awal_2009 >= batas_nilai_2009
 
 print('')
@@ -23,7 +33,7 @@ print(f"ID Token Sinyal: {token_sinyal_2009} | Tipe: {type(token_sinyal_2009)}")
 
 print('')
 print("=== STATUS KELULUSAN PRAKTIKUM ===")
-print(f"Batas Minimun Nilai: {batas_nilai_2009}")
+print(f"Batas Minimun Nilai: {batas_nilai_2009} | Tipe: {type(batas_nilai_2009)}")
 print(f"Apakah Dinyatakan lulus? {hasil_kelulusan_2009} | Tipe: {type(hasil_kelulusan_2009)}")
 
 
